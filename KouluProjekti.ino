15a0f5a908d4a533136227e324cfccd72fec81c0
SakRoi <103430139+SakRoi@users.noreply.github.com>
2022-11-08 09:26:56 +0200

    Added boilerplate code and first subfunctions
    added boilerblate arduino code and added first subfunctions (void servoMoottori, int aaniTutka, void ilmoitusSahkoposti, void ilmoitusLCD)

---
+++ KouluProjekti.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  //tutkan kalibrointi
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void servoMoottori(){
+  
+}
+
+int aaniTutka(){
+  
+}
+
+void ilmoitusSahkoposti(){
+  
+}
+
+void ilmoitusLCD(){
