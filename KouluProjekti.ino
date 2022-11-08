3eae39c9abf1beb5fcdcd5c6366c57b05452315e
SakRoi <103430139+SakRoi@users.noreply.github.com>
2022-11-08 11:10:42 +0200

    added LCD
    Added LiquidCrystal library and basic "calibration" text in the void setup() function in preparation for the sensor calibration.

--- KouluProjekti.ino
+++ KouluProjekti.ino
@@ -1,6 +1,22 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en =11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //Reserving digital pins 2-7 for lcd
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //intializing the LiquidCrystal library by associating the pins
+
 void setup() {
-  // put your setup code here, to run once:
-  //tutkan kalibrointi
+  //setting the LCD's colums and rows (16 columns, 2 rows)
+  lcd.begin(16, 2);
+  //tell the user that we're calibrating the sensor
+  lcd.print("calibrating");
+  delay(10000); //remove when the calibration code is written!!!!
+  lcd.clear();
+  //calibrating the sensor
+  //telling the user that calibration is done
+  lcd.print("calibration");
+  lcd.setCursor(0, 1);
+  lcd.print("is done");
+  delay(10000);
+  lcd.clear();
 
 }
 
@@ -22,3 +38,5 @@ void ilmoitusSahkoposti(){
 }
 
 void ilmoitusLCD(){
+  
+}
