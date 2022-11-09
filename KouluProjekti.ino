5ea0013d96865021fc1a217f8cdc86b012b61fb0
Kurkkumopo666 <78476744+Kurkkumopo666@users.noreply.github.com>
2022-11-09 09:53:32 +0200

    Update KouluProjekti.ino

--- KouluProjekti.ino
+++ KouluProjekti.ino
@@ -1,6 +1,12 @@
+Servo myservo;        //servo object to control a servo
+int servoPos = 0;    //variable to store the servo position
+
+
+
 void setup() {
   // put your setup code here, to run once:
   //tutkan kalibrointi
+  myservo.attach(11);   //attaches the servo on pin 11 to the servo object
 
 }
 
@@ -10,7 +16,14 @@ void loop() {
 }
 
 void servoMoottori(){
-  
+  for (servoPos = 0; servoPos <= 180; servoPos += 1) {  // goes from 0 degrees to 180 degrees
+    myservo.write(servoPos);                           // tell servo to go to position in variable 'servoPos'
+    delay(40);                                        // waits 40ms for the servo to reach the position
+  }
+  for (servoPos = 180; servoPos >= 0; servoPos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(servoPos);                          // tell servo to go to position in variable 'servoPos'
+    delay(40);                                       // waits 40ms for the servo to reach the position
+  }
 }
 
 int aaniTutka(){
