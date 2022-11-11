fdf854c310d5ada9455f3437b7f74fe905a6360f
Kurkkumopo666 <78476744+Kurkkumopo666@users.noreply.github.com>
2022-11-11 10:25:58 +0200

    Update KouluProjekti.ino

--- KouluProjekti.ino
+++ KouluProjekti.ino
@@ -36,13 +36,13 @@ void loop() {
 }
 
 void servoMoottori(){
-  for (servoPos = 0; servoPos <= 180; servoPos += 1) {  // goes from 0 degrees to 180 degrees
+  for (servoPos = 0; servoPos <= 180; servoPos += 15) {  // goes from 0 degrees to 180 degrees
     myservo.write(servoPos);                           // tell servo to go to position in variable 'servoPos'
-    delay(40);                                        // waits 40ms for the servo to reach the position
+    delay(1000);                                        // waits 40ms for the servo to reach the position
   }
-  for (servoPos = 180; servoPos >= 0; servoPos -= 1) { // goes from 180 degrees to 0 degrees
+  for (servoPos = 180; servoPos >= 0; servoPos -= 15) { // goes from 180 degrees to 0 degrees
     myservo.write(servoPos);                          // tell servo to go to position in variable 'servoPos'
-    delay(40);                                       // waits 40ms for the servo to reach the position
+    delay(1000);                                       // waits 40ms for the servo to reach the position
   }
 }
 
