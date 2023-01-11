c8349d5fb8798882b054272a23e7f95312dd5840
Saku Roininen <103430139+SakRoi@users.noreply.github.com>
2023-01-11 18:15:14 +0200

    Updated ESP3866 code
    - removed any code doing serial printing due it conflicting with using RX pin as a GPIO
    - added code which sets RX pin to GPIO and Input
    - re-added the control statements for the email sending

--- esp8266httprequest/esp8266httprequest.ino
+++ esp8266httprequest/esp8266httprequest.ino
@@ -12,49 +12,26 @@ String url = "";
 
 
 void setup() {
-  Serial.begin(115200);
+  //connect to WiFi
   WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    // If WiFi has not been connected
-    // Dots will be printed to serial
-    delay(500);
-    Serial.print(".");
-  }
 
-  // Once connection has been established,
-  // You will be greeted with a message
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-
-  // Can be removed if you want your IP hidden
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  //setting up RX pin to be an GPIO pin, then setting it to be input
+  pinMode(3, FUNCTION_3);
+  pinMode(3, INPUT);
 }
 
 
 void loop() {  
-    int input = digitalRead(2);
-    if (input == HIGH) {      
+  int input = digitalRead(3);
+  if (input == HIGH) {      
     if (WiFi.status() == WL_CONNECTED) {
       WiFiClientSecure client;
       client.setInsecure();
-      
       HTTPClient https;
-      Serial.println("Requesting " + url);
       if (https.begin(client, url)) {
         int httpCode = https.GET();
-        Serial.println("============== Response code: " + String(httpCode));
-        if (httpCode > 0) {
-          Serial.println(https.getString());
-        }
         https.end();
-      } else {
-        Serial.printf("[HTTPS] Unable to connect\n");
-      }
     }
-    delay(2000);
+  delay(2000);
   }
 }
